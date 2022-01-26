99f35d0284fcd3b1b05d6d15212563734e56b27c
Dustm00n <gerardoserranosoto@gmail.com>
2022-01-26 16:15:15 +0000

    avance

---
+++ index.js
@@ -0,0 +1,11 @@
+let PaymentForm = document.querySelector(#PaymentForm);
+
+PaymentForm.addEvenlistener("sumit",(e)=>{
+    e.preventdefault();
+
+    const form = e.target;   
+    if(form.card.value ===""){
+        
+    }
+    
+})
