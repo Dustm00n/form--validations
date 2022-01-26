5dd6b60e9341d6ed4377c1416a399f388aba261d
Dustm00n <gerardoserranosoto@gmail.com>
2022-01-26 20:35:01 +0000

    avance 2

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let PaymentForm = document.querySelector(#PaymentForm);
 
-PaymentForm.addEvenlistener("sumit",(e)=>{
+PaymentForm.addEvenlistener("submit",(e)=>{
     e.preventdefault();
 
+
+
+    let solonumeros = /^[0-9]$/;
+    let checkCVC= /^[0-9]{3}$/;
+    let letras = /^[a-zA-Z/s]+$/;
+
+
     const form = e.target;   
-    if(form.card.value ===""){
-        
+
+
+    if(form.Card.value ==="" || !solonumeros.test(form.Card.value)){
+        alert("por favor verifique el numero de la tarjeta...:)")
+    }
+
+    if(form.CVC.value ==="" ||  !checkCVC.test(form.CVC.value)){
+        alert("por favor verifique el numero de seguridad (CVC)")
+
     }
     
 })
